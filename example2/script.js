a197c6818321765f9bdf85467153db7bcd6643ea
Jacopo Grazzini <gjacopo@gmail.com>
2018-02-10 13:11:07 +0100

    split title dependency in script

--- example2/script.js
+++ example2/script.js
@@ -24,40 +24,6 @@ var outerRadius = Math.min(width, height) / 2  - (mobileScreen ? 80 : 100),
 //How many pixels should the two halves be pulled apart
 var pullOutSize = (mobileScreen? 20 : 50)
 
-//////////////////////////////////////////////////////
-//////////////////// Titles on top ///////////////////
-//////////////////////////////////////////////////////
-
-var titleWrapper = svg.append("g").attr("class", "chordTitleWrapper"),
-	titleOffset = mobileScreen ? 15 : 40,
-	titleSeparate = mobileScreen ? 30 : 0;
-
-//Title	top left
-titleWrapper.append("text")
-	.attr("class","title left")
-	.style("font-size", mobileScreen ? "12px" : "16px" )
-	.attr("x", (width/2 + margin.left - outerRadius - titleSeparate))
-	.attr("y", titleOffset)
-	.text("ICW indicators");
-titleWrapper.append("line")
-	.attr("class","titleLine left")
-	.attr("x1", (width/2 + margin.left - outerRadius - titleSeparate)*0.6)
-	.attr("x2", (width/2 + margin.left - outerRadius - titleSeparate)*1.4)
-	.attr("y1", titleOffset+8)
-	.attr("y2", titleOffset+8);
-//Title top right
-titleWrapper.append("text")
-	.attr("class","title right")
-	.style("font-size", mobileScreen ? "12px" : "16px" )
-	.attr("x", (width/2 + margin.left + outerRadius + titleSeparate))
-	.attr("y", titleOffset)
-	.text("Dimensions");
-titleWrapper.append("line")
-	.attr("class","titleLine right")
-	.attr("x1", (width/2 + margin.left - outerRadius - titleSeparate)*0.6 + 2*(outerRadius + titleSeparate))
-	.attr("x2", (width/2 + margin.left - outerRadius - titleSeparate)*1.4 + 2*(outerRadius + titleSeparate))
-	.attr("y1", titleOffset+8)
-	.attr("y2", titleOffset+8);
 	
 ////////////////////////////////////////////////////////////
 /////////////////// Animated gradient //////////////////////
