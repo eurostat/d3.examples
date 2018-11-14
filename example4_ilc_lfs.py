a59445854bc8f314be5565d0266aa223475025b3
gjacopo <gjacopo@gmail.com>
2018-11-14 10:31:07 +0100

    Update example4_ilc_lfs.py

--- example4_ilc_lfs.py
+++ example4_ilc_lfs.py
@@ -24,7 +24,7 @@ import build_chord
 INDICATOR_CROSS = [[r'^ilc'],[r'^lfs']]
 INDICATOR_KEEP  = reduce(lambda x,y: x + y, INDICATOR_CROSS)
 
-DIMENSION_DROP  = ['time']
+DIMENSION_DROP  = ['time','geo']
 
 INDICATORxDIMENSION = '%sx%s' % (metadata.INDICATOR, metadata.DIMENSION)
 ODIR            = '../__temp__'
@@ -33,8 +33,9 @@ df = metadata.meta2data(ind= {'keep': INDICATOR_KEEP},
                         dim= {'drop': DIMENSION_DROP})
 
 # select some of interest
-x = df[metadata.DIMENSION].isin(['nace_r2', 'nace_r1', 'deg_urb'])
-SELIND = df[x][metadata.INDICATOR].unique()
+SELIND = []
+#x = df[metadata.DIMENSION].isin(['nace_r2', 'nace_r1', 'deg_urb'])
+#SELIND = df[x][metadata.INDICATOR].unique()
 x, y, z =                                   \
     df[metadata.DIMENSION]=='geo',          \
     df[metadata.LABEL].str.len()>2,         \
@@ -43,13 +44,30 @@ selind1 = df[x & y & ~z][metadata.INDICATOR].unique()
 SELIND = list(set(SELIND).union(set(selind1)))
 
 # filter
-x, y =                                      \
-    df[metadata.DIMENSION]=='geo',          \
-    df[metadata.INDICATOR].isin(SELIND)
-df = df[~x & y]
+#x, y =                                      \
+#    df[metadata.DIMENSION]=='geo',          \
+#    df[metadata.INDICATOR].isin(SELIND)
+#df = df[~x & y]
+SELIND = df[metadata.INDICATOR].unique()
+df = df[df[metadata.INDICATOR].isin(SELIND)]
 
 [ind0, ind1, df_cross] = build_chord.meta2cross(df, cross=INDICATOR_CROSS)
 
-build_chord.adjacency2json(df_cross, dim=ind0, ind=ind1,
-                           oifn = metadata.INDICATOR, odfn = metadata.DIMENSION, 
-                           oixdfn = INDICATORxDIMENSION, odir=ODIR)
+df_edges = build_chord.adjacency2edges(df_cross, ind0, ind1)
+
+#build_chord.adjacency2json(df_cross, dim=ind0, ind=ind1,
+#                           oifn = metadata.INDICATOR, odfn = metadata.DIMENSION, 
+#                           oixdfn = INDICATORxDIMENSION, odir=ODIR)
+
+MINCOMMON = 15
+reduced_df_edges = df_edges[df_edges['value']>=MINCOMMON]
+ilc_ind, lfs_ind =  \
+    reduced_df_edges['source'].unique(), reduced_df_edges['target'].unique()
+
+
+lfs_new_ind = ['lfsa_ergaed', 'lfsa_argaed', 'lfsa_ewpshi', 'lfsa_ehomp', 'lfsq_urgan', 'lfsa_urgacob', 'lfsa_pgacws', 'lfsa_ergacob', 'lfsa_urgan', 'lfsa_ergan', 
+ 'lfsa_ewpsun', 'lfsa_ewpsat', 'lfsa_ewpeve', 'lfsa_upgacob', 'lfsa_argacob', 'lfsa_upgan', 'lfsa_argan']
+
+reduced_df_edges = reduced_df_edges[reduced_df_edges['target'].isin(lfs_new_ind)]
+ilc_ind, lfs_ind =  \
+    reduced_df_edges['source'].unique(), reduced_df_edges['target'].unique()
